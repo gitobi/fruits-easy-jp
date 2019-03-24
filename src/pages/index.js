0d86f390a80b1e90cc068ad21fe18eca4b42f57e
Rui Onodera <deraru@gmail.com>
2019-03-24 22:52:18 +0900

    Add layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Fruits Plant by Gitobi</div>
+export default () => (
+  <Layout>
+    <div>Fruits Plant by Gitobi</div>
+  </Layout>
+)
