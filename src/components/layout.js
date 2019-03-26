3e591adca918a83eccfc2298b583ae72b80f8962
Rui Onodera <deraru@gmail.com>
2019-03-26 12:54:12 +0900

    Add title and lang

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,11 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 export default ({ children }) => (
   <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <Helmet>
+      <html lang="ja" />
+    </Helmet>
     {children}
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 
 export default () => (
   <Layout>
+    <Helmet>
+      <title>Fruits Plant by Gitobi</title>
+    </Helmet>
     <div>Fruits Plant by Gitobi</div>
   </Layout>
 )
