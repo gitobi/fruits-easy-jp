05955aaf275551221bd95f8705bd4c93350f4900
Rui Onodera <deraru@gmail.com>
2019-03-26 12:57:54 +0900

    Add description

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export default () => (
   <Layout>
     <Helmet>
       <title>Fruits Plant by Gitobi</title>
+      <meta name="description" content="フルーツ工場メディア" />
     </Helmet>
     <div>Fruits Plant by Gitobi</div>
   </Layout>
