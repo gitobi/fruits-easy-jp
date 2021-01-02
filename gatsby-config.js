71148fe01722adc2ce168c144f3d810a222df26c
Rui Onodera <deraru@gmail.com>
2021-01-02 22:50:45 +0900

    Rename package name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: `Fruit Factory`,
+    description: `Fruit factory media.`,
+  },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`
