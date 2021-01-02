d94b5c70722bc601e7b823f2eeaf8691055a9d91
Rui Onodera <deraru@gmail.com>
2021-01-02 20:59:44 +0900

    Update Gatsby to 2.9.0

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Head from "../components/head"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
+
+export default () => (
+  <Layout>
+    <Head description="フルーツ工場メディア" />
+    <Link to="/">ホーム</Link>
+    <Link to="/about/">Fruits Plantとは</Link>
+    <Link to="/contact/">コンタクト</Link>
+    <div>
+      <h1>お探しのページが見つかりませんでした。</h1>
+    </div>
+  </Layout>
+)
